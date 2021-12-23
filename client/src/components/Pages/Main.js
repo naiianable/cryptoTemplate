de3c24bf6d6d4a6aa490d05af44a7dfa1639164a
Ian Yamashita <ianyamashita@Ians-Air.lan>
2021-12-22 16:53:32 -1000

    some  title, intro styling

--- client/src/components/Pages/Main.js
+++ client/src/components/Pages/Main.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Title from "./Title/Title";
+import Intro from "./Intro";
 
 const Main = () => {
 	return (
 		<div>
 			<Navbar />
+			<Title />
+			<Intro />
 		</div>
 	);
 };
